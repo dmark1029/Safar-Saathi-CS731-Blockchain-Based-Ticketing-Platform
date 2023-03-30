95e950d9ed3560fe18ca5b5c74c7e0052a44976e
YASHRAJMITTAL <rajmittalyash@gmail.com>
2023-03-30 17:16:32 +0530

    Added transfer

--- contracts/Ticketsssssss.sol
+++ contracts/Ticketsssssss.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: UNLICENSESED
 pragma solidity ^0.8.19;
 
+
 contract EventFactory{
-    
     mapping (uint => Event) public events;
     uint public eventIndex;
+    uint[] ats;
 
     struct User{
         string name;
@@ -20,7 +21,6 @@ contract EventFactory{
         uint eventId;
         bool isSold;
     }
-    
     struct Event{
         address payable owner;
         uint eventId;
@@ -29,6 +29,8 @@ contract EventFactory{
         uint numTickets;
         uint unSoldTickets;
         uint ticketPrice;
+        string src;
+        string dest;
         mapping(uint => Ticket) tickets;
     }
 
@@ -43,18 +45,18 @@ contract EventFactory{
         users[msg.sender].doesExist = false;
     }
 
-    
-    function createEvent(string memory _name, uint _date, uint _numTickets, uint _ticketPrice) public{
+    function createEvent(string memory _name, uint _numTickets, uint _ticketPrice, string memory _src, string memory _dest) public{
         require(users[msg.sender].doesExist && users[msg.sender].isProvider);
         Event storage e = events[eventIndex];
         e.owner = payable(msg.sender);
         e.eventId = eventIndex;
         e.name = _name;
-        e.date = _date;
+        e.date = block.timestamp;
         e.numTickets = _numTickets;
         e.unSoldTickets = _numTickets;
-        e.ticketPrice = _ticketPrice;
-
+        e.ticketPrice = _ticketPrice*10**18;
+        e.src = _src;
+        e.dest = _dest;
         for(uint i=1; i<=_numTickets; i++){
             Ticket storage newTicket = e.tickets[i];
             newTicket.owner = payable(msg.sender);
@@ -63,6 +65,21 @@ contract EventFactory{
             newTicket.isSold = false;
         }
         eventIndex++;
+    }
+
+    function showAvailableEvents(string memory _src, string memory _dest) public returns(uint[] memory){
+        delete ats;
+        uint[] storage avail = ats;
+        for(uint i=0; i<eventIndex; i++){
+            if(keccak256(abi.encodePacked((_src))) == keccak256(abi.encodePacked((events[i].src))) && keccak256(abi.encodePacked((_dest))) == keccak256(abi.encodePacked((events[i].dest)))){
+                avail.push(i);
+            }
+        }
+        uint[] memory available = new uint[](avail.length);
+        for(uint i=0; i<avail.length; i++){
+            available[i] = avail[i];
+        }
+        return available;
     }
 
     function showAvailableTickets(uint _eventId) public view returns(uint[] memory){
@@ -77,15 +94,35 @@ contract EventFactory{
         }
         return avail;
     }
-    
-    function buyTicket(uint _eventId, uint _ticketNo) public{
+    function buyTicket(uint _eventId, uint _ticketNo) public payable{
         require(users[msg.sender].doesExist);
         Event storage e = events[_eventId];
-        require(_ticketNo<e.numTickets);
-        require(!e.tickets[_ticketNo].isSold);
+        require(currentPrice(_eventId) <= msg.value, "The Price of Ticket doesn't match");
+        require(_ticketNo<e.numTickets, "Ticket No. doesn't exist");
+        require(!e.tickets[_ticketNo].isSold, "This ticket is soldout choose another seat");
         e.tickets[_ticketNo].isSold = true;
         e.unSoldTickets--;
         e.tickets[_ticketNo].owner = payable(msg.sender);
+    }
+
+    function currentPrice(uint _eventId) public view returns(uint){
+        Event storage e = events[_eventId];
+        if(e.unSoldTickets > 9*e.numTickets/10){
+            return e.ticketPrice;
+        }
+        else if(e.unSoldTickets > 8*e.numTickets/10){
+            return 11*e.ticketPrice/10;
+        }
+        else if(e.unSoldTickets > 7*e.numTickets/10){
+            return 12*e.ticketPrice/10;
+        }
+        else if(e.unSoldTickets > 6*e.numTickets/10){
+            return 13*e.ticketPrice/10;
+        }
+        else if(e.unSoldTickets > 5*e.numTickets/10){
+            return 14*e.ticketPrice/10;
+        }
+        else return 15*e.ticketPrice/10;
     }
 
     function sellTicket(uint _eventId, uint _ticketNo) public payable{
@@ -97,5 +134,16 @@ contract EventFactory{
         e.tickets[_ticketNo].isSold = false;
         e.unSoldTickets++;
         e.tickets[_ticketNo].owner = e.owner;
+        payable(msg.sender).transfer(3*e.ticketPrice/10);
+    }
+
+    function recoverFund(uint _eventId) public payable{
+        Event storage e = events[_eventId];
+        require(e.owner == msg.sender);
+        payable(msg.sender).transfer((e.numTickets - e.unSoldTickets)*e.ticketPrice);
+    }
+
+    function bal() public view returns(uint){
+        return address(this).balance;
     }
 }
