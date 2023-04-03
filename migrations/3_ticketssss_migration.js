cf747cc703c48eafeee474d42525989cb3dce7fc
sandeepb20 <sandeepb20@iitk.ac.in>
2023-04-03 14:26:56 +0530

    Delete useless files

--- migrations/3_ticketssss_migration.js
+++
@@ -1,5 +0,0 @@
-const EventFactory = artifacts.require("EventFactory");
-
-module.exports = function (deployer){
-    deployer.deploy(EventFactory);
-};
--- other/2_demo_migration.js
+++
@@ -1,5 +0,0 @@
-const demo = artifacts.require("demo");
-
-module.exports = function (deployer){
-    deployer.deploy(demo);
-};
